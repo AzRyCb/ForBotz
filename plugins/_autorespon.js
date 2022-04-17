8e1b2f0c0d6136790be5eaa18ce70f6b2ad2631f
Azry <azry695@gmail.com>
2022-04-17 19:53:34 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -28,7 +28,7 @@ handler.all = async function (m, { isBlocked }) {
 
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-        this.send2Button(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `
+        this.send2Button(m.chat, await (await fetch(logo)).buffer(), `
 ╭═══ 〘 SEWA 〙
 ║ ┅ ๑————————————๑
 ║┊ ⌲ Sewa = 5k/grup (1 minggu)
